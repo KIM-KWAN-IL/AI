005969231b86c7e29d0dda55ff86e9d4343632ac
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2024-04-24 12:42:52 +0900

    pydantic model create

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import extract_key_phrase
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -15,31 +17,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# # 데이터베이스 세션 의존성 주입을 위한 함수 추가
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
-### 데이터 유효성을 확인하는 프로세스라고 함. 일단 1차적으로 통신이 문제없는지 확인할 때는 우선순위에서 밀린다고 생각해서 생략
-# from pydantic import BaseModel
-# # 유저 역할
-# class Role(str, Enum): 
-#     admin = "admin"
-#     user = "user"
-#     student = "student"
+### pydantic 정의
+class DiaryRequest(BaseModel):
+    
+    member_id: int
+    diary : str
 
-
-# class User(BaseModel):
-#     # Optional : 필수적으로 필요한 것은 아님
-#     id : Optional[UUID] = uuid4() # UUID : 범용 공유 식별자(Universally unique indentifier)
-#     first_name : str
-#     last_name : str
-#     middle_name : Optional[str]
-#     gender : Gender # class로 정의되어 있음
-#     roles : List[Role]
-
-@app.get("/diary")
-async def root(query: str = Query(None, description="Enter a test string")):
+@app.post("/diary/")
+async def analyze_sentiment(request: DiaryRequest):
     # 응답으로 사용자로부터 받은 쿼리 파라미터 값을 반환
-    if query is not None:
+    if request.diary is not None:
         try:
-            result = sentiment_analysis.sample_classify_document_multi_label(query)
+            result = sentiment_analysis.sample_classify_document_multi_label(request.diary)
             # 반환된 결과를 JSON 응답으로 반환
             return result
         except Exception as e:
@@ -52,12 +49,12 @@ async def root(query: str = Query(None, description="Enter a test string")):
     # create_diary_entry(db, reply=result)
     # db.close()
 
-@app.get("/keyphrase")
-async def root(query: str = Query(None, description="Enter a test string")):
+@app.post("/keyphrase/")
+async def extract_phrases(request: DiaryRequest):
     # 응답으로 사용자로부터 받은 쿼리 파라미터 값을 반환
-    if query is not None:
+    if request.diary is not None:
         try:
-            result = extract_key_phrase.sample_extract_key_phrases(query)
+            result = extract_key_phrase.sample_extract_key_phrases(request.diary)
             # 반환된 결과를 JSON 응답으로 반환
             return result
         except Exception as e:
@@ -65,13 +62,6 @@ async def root(query: str = Query(None, description="Enter a test string")):
         
     else:
         return {"message": "Please provide a query parameter."}
-
-# @app.get("/{name}")
-# async def generate_id_for_name(name: str):
-#     return JSONResponse({
-#         'id': str(uuid.uuid4()),
-#         'name': name
-#     })
 
 
 import uvicorn
